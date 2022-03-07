31bc7486ed1cb3b0156696b56d22d8cc728f49e1
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-08 00:20:29 +0300

    Create refresh service

---
+++ src/services/auth/Refresh.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+
+const jwt = require('jsonwebtoken');
+
+const { User, RefreshToken } = require('../../models');
+
+const MongooseService = require('../Mongoose');
+
+const { generateToken } = require('../../utils/tokenGenerator');
+
+class Refresh {
+  constructor() {
+    this.mongooseUser = new MongooseService(User);
+    this.mongooseRefreshToken = new MongooseService(RefreshToken);
+  }
+
+  async Refresh(body) {
+    const bodyIn = body;
+
+    const refreshToken = await this.mongooseRefreshToken.get({
+      token: bodyIn.refreshToken,
+    });
+
+    if (!refreshToken)
+      return { success: false, error: 'refresh token could not find' };
+
+    const userId = jwt.verify(
+      refreshToken.token,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const accessToken = generateToken(
+      userId,
+      process.env.ACCESS_TOKEN_SECRET,
+      '15d'
+    );
+    return { accessToken, success: true };
+  }
+}
+
+module.exports = Refresh;
