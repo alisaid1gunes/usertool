c2f0e4873f46018f09960b81177ba1088cfea0dd
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-08 00:23:42 +0300

    Create refresh controller

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -3,8 +3,12 @@ const register = require('./register');
 const login = require('./login');
 
 const logout = require('./logout');
+
+const refresh = require('./refresh');
+
 module.exports = {
   register,
   login,
   logout,
+  refresh
 };
---
+++ src/controllers/auth/refresh.js
@@ -0,0 +1,24 @@
+/* eslint-disable consistent-return */
+const { StatusCodes } = require('http-status-codes');
+
+const ApiErrorService = require('../../services/ApiError');
+
+const { Refresh } = require('../../services/auth');
+
+const RefreshService = new Refresh();
+
+const refresh = async (req, res, next) => {
+  try {
+    const result = await RefreshService.Refresh(req.body);
+
+    if (result.success) return res.status(StatusCodes.OK).send(result);
+
+    next(ApiErrorService.badRequest(result.error));
+  } catch (err) {
+    next(
+      ApiErrorService.badRequest('Token could not refresh. Request is wrong')
+    );
+  }
+};
+
+module.exports = refresh;
