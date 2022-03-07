7fae1445c882d6a4ddcf57b1bfb07536c7aeb9b7
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-07 16:53:56 +0300

    Create register controller

---
+++ src/controllers/auth/register.js
@@ -0,0 +1,26 @@
+/* eslint-disable consistent-return */
+const { StatusCodes } = require('http-status-codes');
+
+const ApiErrorService = require('../../services/ApiError');
+
+const { Register } = require('../../services/auth');
+
+const RegisterService = new Register();
+
+const register = async (req, res, next) => {
+  try {
+    const result = await RegisterService.RegisterUser(req);
+
+    if (result.success) return res.status(StatusCodes.CREATED).send(result);
+
+    next(ApiErrorService.badRequest(result.error));
+  } catch (err) {
+    next(
+      ApiErrorService.badRequest(
+        'User could not register. Request is wrong'
+      )
+    );
+  }
+};
+
+module.exports = register;
