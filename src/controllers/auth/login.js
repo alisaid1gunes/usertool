88835b9659fb968a39ab1a5ae8a6f165723c3e6d
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-08 00:12:36 +0300

    Create login controller

---
+++ src/controllers/auth/login.js
@@ -0,0 +1,30 @@
+/* eslint-disable consistent-return */
+const { StatusCodes } = require('http-status-codes');
+
+const ApiErrorService = require('../../services/ApiError');
+
+const { Login } = require('../../services/auth');
+
+const LoginService = new Login();
+
+const login = async (req, res, next) => {
+  try {
+    const result = await LoginService.LoginUser(req.body);
+
+    if (result.success) {
+      const { accessToken } = result;
+      const { refreshToken } = result;
+      return res.header('auth-token', accessToken).status(StatusCodes.OK).send({
+        accessToken,
+        refreshToken,
+        error: result.error,
+        succes: result.success,
+      });
+    }
+    next(ApiErrorService.unauthorized(result.error));
+  } catch (err) {
+    next(ApiErrorService.unauthorized(`User did not log in.${err}`));
+  }
+};
+
+module.exports = login;
