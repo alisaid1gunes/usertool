1ddedf42f0b90331e91d6fd0aadad5263778a4d7
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-07 15:11:30 +0300

    Create user model

---
+++ src/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    min: 3,
+    max: 10,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    min: 8,
+    max: 16,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  surname: {
+    type: String,
+    require: true,
+  },
+  age: {
+    type: Number,
+    require: true,
+  },
+  bornAt: {
+      type: Date,
+      require: true,
+  },
+  location: {
+    type: Geolocation,
+    require: true,
+  },
+  about: {
+    type: String,
+    require: true,
+  },
+  balance: {
+    type: Number,
+    require: true,
+  },
+  phoneNumber: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
