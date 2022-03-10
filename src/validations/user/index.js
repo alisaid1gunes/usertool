39443e7b7fb7ff3bff1f2da912b2511d455f006d
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-10 16:56:52 +0300

    Create user validation validation

--- src/validations/user/index.js
+++ src/validations/user/index.js
@@ -2,6 +2,8 @@ const getValidation = require('./get');
 
 const getAllValidation = require('./getAll');
 
+const updateValidation = require('./updateValidation');
+
 module.exports = {
   getValidation,
   getAllValidation,
---
+++ src/validations/user/update.js
@@ -0,0 +1,37 @@
+const Joi = require('joi-oid');
+
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
+const update = (data) => {
+  const userSchema = Joi.object({
+    username: Joi.string().min(3).max(10),
+    email: Joi.string().email(),
+    password: Joi.string().min(8).max(10),
+    image: Joi.string(),
+    name: Joi.string(),
+    surname: Joi.string(),
+    age: Joi.number(),
+    bornAt: Joi.date(),
+    location: Joi.object({
+      type: pointSchema,
+      name: Joi.string(),
+      coordinates: Joi.array(),
+    }),
+    about: Joi.string(),
+    balance: Joi.number(),
+    phoneNumber: Joi.string(),
+  });
+  return userSchema.validate(data);
+};
+
+module.exports = update;
