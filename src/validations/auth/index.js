1a996c4c7f64b889f9e7823c688eff598f9c1f2f
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-10 16:44:15 +0300

    Create logout validation

--- src/validations/auth/index.js
+++ src/validations/auth/index.js
@@ -2,4 +2,6 @@ const register = require('./register');
 
 const login = require('./login');
 
-module.exports = { register, login };
+const logout = require('./logout');
+
+module.exports = { register, login, logout };
