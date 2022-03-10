15369afb351c365d0caee729503810bde1a2737d
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-10 16:49:17 +0300

    create user get validation

---
+++ src/validations/user/get.js
@@ -0,0 +1,5 @@
+const Joi = require('joi-oid');
+
+const get = (data) => Joi.objectId().required().validate(data);
+
+module.exports = get;
---
+++ src/validations/user/index.js
@@ -0,0 +1,5 @@
+const getValidation = require('./get');
+
+module.exports = {
+  getValidation,
+};
